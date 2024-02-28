24019877f1a0e29a92b95685a51f33a685bf63f7
skoniks <skolya16@ya.ru>
2024-02-28 13:25:27 +0300

    Clean config update

--- rimraf.config.js
+++ rimraf.config.js
@@ -1,7 +1,7 @@
 (async () => {
   const { globbySync } = await import('globby');
   const { rimrafSync } = await import('rimraf');
-  const patterns = ['proto/*', '!proto/index.ts'];
+  const patterns = ['proto/*', '!proto/index.ts', '!proto/rollup.d.ts'];
   const paths = globbySync(patterns, { onlyFiles: false });
   for (const path of paths) rimrafSync(path);
 })();
